65bb67443dc29af6890ede85a40b8359291a9820
Faisal <faq01818@gmail.com>
2018-10-21 19:10:12 +0530

    S16 L157, Creating the Server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const config = require('./config/config').get(process.env.NODE_ENV)
+
+const app = express()
+
+mongoose.promise = global.promise
+mongoose.connect(config.DATABASE)
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+
+const port = process.env.PORT || 3001
+
+app.listen(port, ()=>{
+    console.log(`Server running at ${port}`)
+})
