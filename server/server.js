aeeffd61602e494f907e28869ff2979082a38f93
Faisal <faq01818@gmail.com>
2018-10-22 23:03:58 +0530

    S16 L161, User Routes

--- server/server.js
+++ server/server.js
@@ -53,6 +53,40 @@ app.post('/api/book', (req,res)=>{
         })
     })
 })
+
+app.post('/api/register', (req,res)=>{
+    const user = new User(req.body)
+    user.save((err, doc)=>{
+        if(err) return res.json({success:false})
+        res.status(200).json({
+            success:true,
+            user:doc
+        })
+    })
+})
+
+app.post('/api/login', (req,res)=>{
+    User.findOne({'email':req.body.email}, (err, user)=>{
+        if(!user) return res.json({isAuth:false, message:'Auth failed, email not found'})
+
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch) return res.json({
+                isAuth:false,
+                message:'Wrong password'
+            })
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err)
+                res.cookie('auth', user.token).send({
+                    isAuth:true,
+                    id:user._id,
+                    email:user.email
+                })
+            })
+        })
+    })
+})
+
+
 // UPDATE //
 app.post('/api/book_update', (req,res)=>{
     
